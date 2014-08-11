24497d38c709930997656a0710b042ff0e871fcc
nmorgan1 <nickmorgan@quickenloans.com>
2014-08-11 14:18:56 -0400

    Allow CORS

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var https = require('https');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function(request, response) {
     response.send("This is the default response.");
     console.log(request.query);
